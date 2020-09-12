b35452523e143e2d111f8741590c516df2cb99de
GitSeob <anhs0220@gmail.com>
2020-09-12 13:10:21 +0900

    실패율 - 100%

--- all/lv1/failure_rate.py
+++ all/lv1/failure_rate.py
@@ -1,16 +1,15 @@
 def solution(N, stages):
-	res = [[i+1, 0] for i in range(N)]
+	dic = {}
+	n = len(stages)
+	for i in range(1,N+1):
+		if n == 0 :
+			dic[i] = 0
+			continue
+		item = stages.count(i)/n
+		n -= stages.count(i)
+		dic[i] = item
+	return sorted(dic, key=lambda k : dic[k], reverse = True)
 
-	for i in range(1, N+1):
-		total = 0
-		clear = 0
-		for stage in stages:
-			if stage >= i:
-				total += 1
-				if stage > i:
-					clear += 1
-		res[i-1][1] = clear/total
-	return [x for x, _ in sorted(res, key=lambda x: x[1])]
 
 n1 = [2, 1, 2, 6, 2, 4, 3, 3]
 n2 = [4, 4, 4, 4, 4]
