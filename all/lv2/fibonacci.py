5452ffb4d626e82a164b9017fc73b97dac28f090
GitSeob <anhs0220@gmail.com>
2020-09-14 11:27:51 +0900

    피보나치의 수

--- all/lv2/fibonacci.py
+++ all/lv2/fibonacci.py
@@ -1,8 +1,8 @@
 def fibonacci(num):
-    a,b = 0,1
-    for _ in range(num):
-        a,b = b,a+b
-    return a
+	a, b = 0,1
+	for _ in range(num):
+		a, b = b, a+b
+	return a
 
 def solution(n):
-    return fibonacci(n) % 1234567
+	return fibonacci(n) % 1234567
